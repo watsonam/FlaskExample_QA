8eafd4022d5c152cdb71323e27128ac3994a0e38
Adam Watson <adam.watson@ihsmarkit.com>
2019-04-08 15:54:55 +0100

    .env changes

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
-#app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 
 @app.route('/')
@@ -16,5 +16,5 @@ def hello_name(name):
     return "Hello {}!".format(name)
 
 if __name__ == '__main__':
-    #print(os.environ['APP_SETTINGS'])
+    print(os.environ['APP_SETTINGS'])
     app.run()
